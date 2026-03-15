ff2690bfba2fcb5593b7189ef83efffae858a27b
APLUniverso <juanfelixbd@gmail.com>
2026-03-15 10:42:14 -0500

    feat: :zap: Mejora del condicional para detectar choques ademas de mejorar la hitbox del gato para evitar choques que no se ven

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const canvas = document.getElementById("gameBoard")
 const ctx = canvas.getContext("2d")
 
+const gap = 200;
+const gravity = 5;
+const tuboSpeed = 10
+const jumpForce = -30
+let velocity = 0
+
+const catWidth = 100;
+const catHeigth = 60;
+
+let cat = {
+    x : 100,
+    y : 100,
+    angle : 0
+}
+
+const limitHeight = 530;
+const rectAngle = 1.5708;
+
 const tuboUp = new Image();
 tuboUp.src = "./media/tuboUp.png"
 const tuboDown = new Image();
@@ -11,6 +29,7 @@ const imgTubo = {
     down: tuboDown
 }
 
+//frames del gato cons lo que lo vamos a animer
 let frames = []
 // cargar frames
 for(let i = 1; i <= 4; i++){;
@@ -19,43 +38,25 @@ for(let i = 1; i <= 4; i++){;
     frames.push(img);
 };
 
-let currentFrame = 0
-
-let cat = {
-    x : 100,
-    y : 100,
-    angle : 0,
-    img : frames[0],
-    width : 100,
-    height : 60 
-}
-
 const tuboWidth = 80;
 const tuboHeight = 300;
 
 let tubosInferiores = [
     {
         x : 700,
-        y : 400,
-        img : imgTubo.down,
-        width : 80,
-        height : 300 
+        y : 400
     },
     {
         x : 700,
-        y : 400,
-        img : imgTubo.down,
-        width : 80,
-        height : 300 
+        y : 400
     },
     {
         x : 700,
-        y : 400,
-        img : imgTubo.down,
-        width : 80,
-        height : 300 
+        y : 400
     }
 ]
+
+//Separamos los tubos y los ponemos a distintas alturas
 const separacion = 300
 tubosInferiores = tubosInferiores.map((t,index) => {
     t.y = Math.random() * (500 - 300) + 300
@@ -63,33 +64,15 @@ tubosInferiores = tubosInferiores.map((t,index) => {
     return {...t}
 })
 
-const gap = 150;
-const gravity = 5;
-let velocity = 0
-const tuboSpeed = 10
-const jumpForce = -30
-
-let tubo = {
-    x : 700,
-    y : 400,
-    img : imgTubo.down,
-    width : 80,
-    height : 300 
-}
-
-const limitHeight = 530;
-const rectAngle = 1.5708;
+let currentFrame = 0
 function animate(){
     velocity += gravity
     ctx.clearRect(0,0,canvas.width,canvas.height)
 
-    
-
     cat.y = cat.y + velocity >= limitHeight ? limitHeight : cat.y += velocity
     cat.angle = cat.angle + 0.08 >= rectAngle ? rectAngle : cat.angle += 0.08
 
-    cat.img = frames[currentFrame]
-    draw(cat);
+    draw(cat,catWidth,catHeigth,frames[currentFrame]);
 
     currentFrame++;
     if(currentFrame >= frames.length){
@@ -103,85 +86,69 @@ function animate(){
             t.x = maxX + 300
             t.y = Math.random() * (500 - 300) + 300
         }
-        draw(t)
-        draw({
+        draw(t,tuboWidth,tuboHeight,imgTubo.down)
+        const tuboSupeior = {
             x : t.x,
-            y : t.y-t.height-gap,
-            img : imgTubo.up,
-            width : 80,
-            height : 300 
-        })
+            y : t.y-tuboHeight-gap,
+        }
+        draw(tuboSupeior,tuboWidth,tuboHeight,imgTubo.up)
     })
 
-    /* const tuboCercano = {
-        down : tubosInferiores
-                .filter(t => t.x + t.width > cat.x)
-                .sort((a, b) => a.x - b.x)[0],
-        up : {
-            x : tuboCercano.down.x,
-            y : tuboCercano.down.y - t.height - gap,
-            width : 80,
-            height : 300 
-        }
-    }
-    console.log(tuboCercano) */
 
-    const tuboCercanoInferior = tubosInferiores
-        .filter(t => t.x + t.width > cat.x)
+    const tuboCercano = tubosInferiores
+        .filter(t => t.x + tuboWidth > cat.x)
         .sort((a, b) => a.x - b.x)[0]
-    /* const tuboCercanoSupeior = {
-            x : t.x,
-            y : t.y-t.height-gap,
-            img : imgTubo.up,
-            width : 80,
-            height : 300 
-        } */
+
+    //reducimos la hitbox del gato para que no se detecten colisiones imposibles
+    const catBox = {
+        x: cat.x + 15,
+        y: cat.y + 10,
+        width: catWidth-30,
+        height: catHeigth-20
+    }
+
+    //para ver la hit box del gato
+    ctx.strokeStyle = "red"
+    ctx.strokeRect(catBox.x, catBox.y, catBox.width, catBox.height)
+
+    const tuboInferior = {
+        ...tuboCercano,
+        width: tuboWidth,
+        height: tuboHeight
+    }
+
+    const tuboSuperior = {
+        x: tuboCercano.x,
+        y: tuboCercano.y - tuboHeight - gap,
+        width: tuboWidth,
+        height: tuboHeight
+    }
 
     if (
-        (tuboCercanoInferior && collision(tuboCercanoInferior, cat)) ||
-        collision(
-            {
-                x:tuboCercanoInferior.x,
-                y:tuboCercanoInferior.y - tuboCercanoInferior.height - gap,
-                width:tuboCercanoInferior.width,
-                height:tuboCercanoInferior.height
-            },
-            cat
-        )
+        tuboCercano &&
+        (collision(tuboInferior, catBox) || collision(tuboSuperior, catBox))
     ) {
         console.log("¡Colisión detectada!")
+    }else{
+        setTimeout(() => {
+            requestAnimationFrame(animate)
+        }, 50);
     }
-
-    setTimeout(() => {
-        requestAnimationFrame(animate)
-    }, 100);
 }
 
-//maneajer los conceptos de velocity gravedad y distnacia para el movimiento del pajaro
-animate();
-
-document.addEventListener("click", ()=>{
-    velocity = jumpForce
-    cat.y -= velocity;
-    cat.angle = -0.4;
-})
-
-function draw(obj){
+function draw(obj,width,height,img){
     ctx.save()
 
-    // mover origen al centro del objeto
-    ctx.translate(obj.x + obj.width/2, obj.y + obj.height/2)
+    ctx.translate(obj.x + width/2, obj.y + height/2)
 
-    // rotar si tiene ángulo
     ctx.rotate(obj.angle ? obj.angle : 0)
 
-    // dibujar desde el centro
     ctx.drawImage(
-        obj.img,
-        -obj.width/2,
-        -obj.height/2,
-        obj.width,
-        obj.height
+        img,
+        -width/2,
+        -height/2,
+        width,
+        height
     )
 
     ctx.restore()
@@ -195,3 +162,12 @@ function collision(a, b){
         a.y + a.height > b.y
     )
 }
+
+animate();
+
+document.addEventListener("click", ()=>{
+    velocity = jumpForce
+    cat.y -= velocity;
+    cat.angle = -0.4;
+})
+
